7996000e4d44a79b0d5851ac71e2d5bfd1634c22
Ankur-Datta-4 <ankurr2002@gmail.com>
2022-06-11 12:27:24 +0530

    pre-setup

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const createError = require('http-errors');
+const morgan = require('morgan');
+const cors=require('cors')
+const mongoose=require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const MONGO_URI='mongodb+srv://me:Ka04mw1613@cluster0.82mqnkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(morgan('dev'));
+
+app.get('/', async (req, res, next) => {
+  res.send({ message: 'Awesome it works 🐻' });
+});
+
+app.use('/api', require('./routes/api.route'));
+
+app.use((req, res, next) => {
+  next(createError.NotFound());
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    status: err.status || 500,
+    message: err.message,
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, async() =>{ 
+  console.log(`🚀 @ http://localhost:${PORT}`);
+  await mongoose.connect(MONGO_URI);
+  console.log(`Connected to DB`);
+
+});
